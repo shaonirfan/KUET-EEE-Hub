3fd9500de10e9d569718c00880032f8dbadbfd67
Irfan Shaon <irfanshaon72@gmail.com>
2025-06-03 10:13:10 +0000

    make it a little bit upper, also why at bottom, this extra black region?

--- src/components/ui/tubelight-navbar.tsx
+++ src/components/ui/tubelight-navbar.tsx
@@ -60,7 +60,7 @@ export function NavBar({ items, className }: NavBarProps) {
   return (
     <div
       className={cn(
-        "fixed bottom-0 sm:top-0 left-1/2 -translate-x-1/2 z-50 mb-6 sm:mt-6 sm:pt-0",
+        "fixed bottom-0 sm:top-0 left-1/2 -translate-x-1/2 z-50 mb-2 sm:mt-2 sm:mb-0 sm:pt-0", // Adjusted margins
         className,
       )}
     >
